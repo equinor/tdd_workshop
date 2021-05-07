4318ad1ba145d78f6c9c18d57373c82e87974cf9
Helena Nandi Formentin <hnfo@equinor.com>
2021-05-07 15:04:07 +0200

    Parametrize test function

--- tests/unit/test_add_string.py
+++ tests/unit/test_add_string.py
@@ -1,29 +1,8 @@
 from operators.add_string import add_string
+import pytest
 
-# def test_two_numbers_are_equal():
-#     assert 1 == 1
 
-# List of tests:
-
-# Test more than 1 number
-def test_two_numbers():
-    input = "1,2"
+@pytest.mark.parametrize("input, expected_result", [("1,2", 1 + 2), ("1", 1), ("", 0)])
+def test_add_string(input, expected_result):
     result = add_string(input)
-    expected_result = 1 + 2
-    assert expected_result == result
-
-
-# Test 1 number
-def test_one_numbers():
-    input = "1"
-    result = add_string(input)
-    expected_result = 1
-    assert expected_result == result
-
-
-# Test input is empty
-def test_empty_string():
-    input = ""
-    result = add_string(input)
-    expected_result = 0
     assert expected_result == result
