d2f8971387fe8a18b2184e40fb651e8959cc7a96
Hallgeir Holien <halh@equinor.com>
2021-09-07 13:36:32 +0200

    sum two numbers

---
+++ operators/sum_two_numbers.py
@@ -0,0 +1,3 @@
+
+def sum_two_numbers(number1, number2):
+    return number1 + number2
---
+++ tests/unit/test_sum.py
@@ -0,0 +1,8 @@
+from operators.sum_two_numbers import sum_two_numbers
+import pytest
+
+@pytest.mark.parametrize("number1, number2, expected", [(1,2,3), (2,2,4)])
+def test_sum_of_two_numbers(number1, number2, expected):
+    result = sum_two_numbers(number1, number2)
+    assert expected == result
+ 
