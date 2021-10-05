b7a655ead0592221673a1e4747e1e4a317625fcf
Espen Stokkereit <ESSTO@equinor.com>
2021-10-05 14:31:42 +0200

    Fixed linting

--- tests/unit/test_add_string.py
+++ tests/unit/test_add_string.py
@@ -1,7 +1,10 @@
 from operators.add_string import add_string
 import pytest
 
-@pytest.mark.parametrize("string_input, target", [("1,2", 3), ("-1,1", 0), ("-1,-2", -3), ("0,1", 1)])
+
+@pytest.mark.parametrize(
+    "string_input, target", [("1,2", 3), ("-1,1", 0), ("-1,-2", -3), ("0,1", 1)]
+)
 def test_add_string(string_input, target):
     result = add_string(string_input)
     assert result == target
