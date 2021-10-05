8243b8b7c38a5fa12750fdb37f4dfbb4e6c9b73d
Janne Myllärinen <janne.i.myllarinen@gmail.com>
2021-10-05 15:14:47 +0200

    Add common progress

--- tests/unit/test_string_to_number.py
+++ tests/unit/test_string_to_number.py
@@ -1,8 +1,9 @@
 # Test with "", "-1", "1", "+1"
 from operators.string_to_number import string_to_int
+import pytest
 
 
-def test_string_to_int():
-    string_input = "1"
+@pytest.mark.parametrize("string_input, target", [("1", 1), ("-1", -1), ("", 0)])
+def test_string_to_int(string_input, target):
     result = string_to_int(string_input)
-    assert result == int("1")
+    assert result == target
