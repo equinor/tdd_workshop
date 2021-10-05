cb6b49c7adc2ba20f497b6880cfde5bd9b7a6f6e
Espen Stokkereit <ESSTO@equinor.com>
2021-10-05 14:38:33 +0200

    Fixed black

--- tests/unit/test_string_to_number.py
+++ tests/unit/test_string_to_number.py
@@ -2,6 +2,7 @@
 from operators.string_to_number import string_to_int
 import pytest
 
+
 @pytest.mark.parametrize("string_input, target", [("1", 1), ("-1", -1), ("", 0)])
 def test_string_to_int(string_input, target):
     result = string_to_int(string_input)
