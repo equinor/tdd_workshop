27569592b5ad153ae446c48f960600ba27b332d7
Espen Stokkereit <ESSTO@equinor.com>
2021-10-05 14:23:58 +0200

    Added new method add_string with test

---
+++ operators/add_string.py
@@ -0,0 +1,5 @@
+def add_string(string_input):
+    numbers = string_input.split(",")
+    number1 = int(numbers[0])
+    number2 = int(numbers[1])
+    return number1 + number2
--- tests/unit/test_add_string.py
+++ tests/unit/test_add_string.py
@@ -1,2 +1,7 @@
-def test_two_numbers_are_equal():
-    assert 1 == 1
+from operators.add_string import add_string
+import pytest
+
+@pytest.mark.parametrize("string_input, target", [("1,2", 3), ("-1,1", 0), ("-1,-2", -3), ("0,1", 1)])
+def test_add_string(string_input, target):
+    result = add_string(string_input)
+    assert result == target
