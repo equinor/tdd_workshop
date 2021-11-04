c3773fc44ace11b6e54d9530beb088da9c99fb27
Øyvind Eide <eide.oyvind87@gmail.com>
2021-11-04 12:30:37 +0100

    Add tests with parameterize

--- tests/unit/test_string_to_number.py
+++ tests/unit/test_string_to_number.py
@@ -1,8 +1,10 @@
 # Test with "", "-1", "1", "+1"
 from operators.string_to_number import string_to_int
 
+import pytest
 
-def test_string_to_int():
-    string_input = "1"
-    result = string_to_int(string_input)
-    assert result == 1
+
+@pytest.mark.parametrize("input_string, expected", [("1", 1), ("-1", -1), ("+1", 1)])
+def test_string_to_int(input_string, expected):
+    result = string_to_int(input_string)
+    assert result == expected
