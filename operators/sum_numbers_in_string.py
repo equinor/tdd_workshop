0eb097aad999e852ac5dc961a092e4aa4d054322
Torstein Lundervold Nesheim <TLNE@equinor.com>
2021-11-04 11:11:32 +0100

    Reformatted files using black

--- operators/sum_numbers_in_string.py
+++ operators/sum_numbers_in_string.py
@@ -1,5 +1,6 @@
 from operators.string_to_number import string_to_int
 
+
 def sum_numbers_in_string(string_input):
-    first_number,second_number = map(string_to_int,string_input.split(','))
+    first_number, second_number = map(string_to_int, string_input.split(","))
     return first_number + second_number
--- tests/unit/test_sum_string_numbers.py
+++ tests/unit/test_sum_string_numbers.py
@@ -1,35 +1,42 @@
 from operators.sum_numbers_in_string import *
 import pytest
 
+
 def test_sum_numbers_in_string():
     string_input = "1,2"
     result = sum_numbers_in_string(string_input)
-    assert result==3
+    assert result == 3
+
 
 def test_empty_string():
     string_input = ""
     with pytest.raises(ValueError):
         sum_numbers_in_string(string_input)
 
+
 def test_none_input():
     string_input = None
     with pytest.raises(AttributeError):
         sum_numbers_in_string(string_input)
+
 
 def test_sum_1_numbers_comma_in_string():
     string_input = "1,"
     with pytest.raises(ValueError):
         sum_numbers_in_string(string_input)
 
+
 def test_sum_1_numbers_in_string():
     string_input = "1"
     with pytest.raises(ValueError):
         sum_numbers_in_string(string_input)
 
+
 def test_sum_too_many_numbers():
     string_input = "1,2,3"
     with pytest.raises(ValueError):
         sum_numbers_in_string(string_input)
+
 
 def test_sum_too_many_commas():
     string_input = "1,2,"
