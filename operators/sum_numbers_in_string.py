c63763ffa9257c94958344724f9583a9fa641c3a
Torstein Lundervold Nesheim <TLNE@equinor.com>
2021-11-04 11:02:37 +0100

    Add function sum_two_numbers_in_string & testing

---
+++ operators/sum_numbers_in_string.py
@@ -0,0 +1,5 @@
+from operators.string_to_number import string_to_int
+
+def sum_numbers_in_string(string_input):
+    first_number,second_number = map(string_to_int,string_input.split(','))
+    return first_number + second_number
