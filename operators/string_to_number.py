d03da5a6ad5520b39cb4694db2be52d98f46c972
Petter Søland <petter.soland@bouvet.no>
2021-11-04 11:15:42 +0100

    Handle comma in string_to_number

--- operators/string_to_number.py
+++ operators/string_to_number.py
@@ -1,2 +1,12 @@
 def string_to_int(string_input):
-    return int(string_input)
+
+    if not string_input:
+        raise ValueError("Converting string to integer failed")
+
+    if "," in string_input:
+        numbers = string_input.split(",")
+        int_output = sum([int(x) for x in numbers])
+    else:
+        int_output = int(string_input)
+
+    return int_output
--- tests/unit/test_string_to_number.py
+++ tests/unit/test_string_to_number.py
@@ -6,3 +6,37 @@ def test_string_to_int():
     string_input = "1"
     result = string_to_int(string_input)
     assert result == 1
+
+
+import pytest
+
+def test_two_numbers_in_string():
+    string_input = "1,2"
+    result = string_to_int(string_input)
+    assert result == 3
+    
+def test_multiple_numbers_in_string():
+    string_input = "1,2,3,4"
+    result = string_to_int(string_input)
+    assert result == 10
+
+def test_empty_to_int():
+    string_input = ""
+    with pytest.raises(ValueError, match = "Converting string to integer failed"):
+        string_to_int(string_input)
+
+
+def test_minus_to_int():
+    string_input = "-1"
+    result = string_to_int(string_input)
+    assert result == -1
+
+def test_plus_to_int():
+    string_input = "+1"
+    result = string_to_int(string_input)
+    assert result == 1
+
+def test_mixed_delimiter_in_string():
+    string_input = "1.2,3-4"
+    result = string_to_int(string_input)
+    assert result == 10
