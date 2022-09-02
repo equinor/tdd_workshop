72004df2a2908484636418215301cd9aa0cd6c50
Helena Nandi Formentin <62240435+hnformentin@users.noreply.github.com>
2022-09-02 10:38:53 +0000

    Add function add_two_numbers_from_string

---
+++ operators/add_string.py
@@ -0,0 +1,3 @@
+def add_two_numbers_from_string(string_to_add):
+    [first_number, second_number] = string_to_add.split(",")
+    return int(first_number) + int(second_number)
--- tests/unit/test_add_string.py
+++ tests/unit/test_add_string.py
@@ -1,2 +1,7 @@
-def test_two_numbers_are_equal():
-    assert 1 == 1
+from operators.add_string import add_two_numbers_from_string
+
+def test_add_two_numbers_from_string():
+    input_string = "1,2"
+    expected_result = 3
+    result = add_two_numbers_from_string(input_string)
+    assert result == expected_result
