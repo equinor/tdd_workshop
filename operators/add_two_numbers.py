b4371f5cf8d7f01c19f9f95a1757cb1f99d8fbbe
Roger Wiss <46483498+rowisequinor@users.noreply.github.com>
2022-09-13 11:14:10 +0000

    format w black

--- operators/add_two_numbers.py
+++ operators/add_two_numbers.py
@@ -1,4 +1,4 @@
 def add_two_numbers(numbers):
     (x, y) = numbers.split(",")
     result = int(x) + int(y)
-    return  result
+    return result
--- tests/unit/test_add_two_numbers.py
+++ tests/unit/test_add_two_numbers.py
@@ -1,4 +1,5 @@
 from operators.add_two_numbers import add_two_numbers
 
+
 def test_addition():
     assert add_two_numbers("1,2") == 3
