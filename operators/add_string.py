806d1d95c460b4ab7cb7df910387fce3867d184d
Frida Erdal <31915755+pomfrida@users.noreply.github.com>
2022-09-13 10:48:17 +0000

    Fix linting

--- operators/add_string.py
+++ operators/add_string.py
@@ -1,5 +1,6 @@
 from operators.string_to_number import string_to_int
 
+
 def add(stringWithNumbers):
     splitted = stringWithNumbers.split(",")
     return string_to_int(splitted[0]) + string_to_int(splitted[1])
