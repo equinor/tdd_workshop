d93f7cc5604d73d6a3c71af0da86f87aa34851fa
Helena Nandi Formentin <62240435+hnformentin@users.noreply.github.com>
2023-09-08 14:06:12 +0000

    Make red test for read_coordinates as float tuples

--- tests/unit/test_read_coordinates.py
+++ tests/unit/test_read_coordinates.py
@@ -9,12 +9,12 @@ def test_that_we_can_split_file_into_coordinates(test_data_path):
     the file located on disk.
     """
     expected_coordinates_list = [
-        ("1", "1"),
-        ("2.0", "1.8"),
-        ("2.5", "2.1"),
-        ("3", "2"),
-        ("4.3", "7.2"),
-        ("5.3", "6.2"),
+        (float("1"), float("1")),
+        (float("2.0"), float("1.8")),
+        (float("2.5"), float("2.1")),
+        (float("3"), float("2")),
+        (float("4.3"), float("7.2")),
+        (float("5.3"), float("6.2")),
     ]
 
     assert read_coordinates(test_data_path / "test_input") == expected_coordinates_list
