50886824f4626fd21717592ed5b89ae7e6d78969
Øyvind Eide <44577479+oyvindeide@users.noreply.github.com>
2023-09-13 12:53:54 +0000

    Add treasure hunt script

--- src/tdd_workshop/io.py
+++ src/tdd_workshop/io.py
@@ -6,7 +6,7 @@ if TYPE_CHECKING:
     from typing import Union, List, Tuple
 
 
-def read_coordinates(file_name: Union[str, Path]) -> List[Tuple[str, str]]:
+def read_coordinates(file_name: Union[str, Path]) -> List[Tuple[float, float]]:
     """
     Will read a file of comma separated strings and turn it
     into a list
@@ -15,5 +15,16 @@ def read_coordinates(file_name: Union[str, Path]) -> List[Tuple[str, str]]:
     with open(file_name, "r", encoding="utf-8") as input_file:
         for line in input_file:
             x, y = line.strip().split(",")
-            coordinates.append((x, y))
+            coordinates.append((float(x), float(y)))
     return coordinates
+
+
+def calculate_slope(x1, x2, y1, y2):
+    return (y2 - y1) / (x2 - x1)
+
+
+def treasure_hunt(input_coordinates):
+    for i, (x2, y2) in enumerate(input_coordinates[1:]):
+        x1, y1 = input_coordinates[i]
+        if calculate_slope(x1, x2, y1, y2) > 1:
+            return x2, y2
--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -12,16 +12,13 @@ def test_that_cli_fails_when_file_is_missing():
 
 def test_that_cli_prints_coordinates(monkeypatch, tmp_path):
     monkeypatch.chdir(tmp_path)
-    expected_coordinates_list = [
+    coordinates_list = [
         ("2.0", "1.9"),
         ("2.5", "1.5"),
         ("3.3", "7.3"),
     ]
     with open("test_input", "w", encoding="utf-8") as fout:
-        fout.write("\n".join(",".join(coords) for coords in expected_coordinates_list))
+        fout.write("\n".join(",".join(coords) for coords in coordinates_list))
     runner = CliRunner()
     result = runner.invoke(main, ["test_input"])
-    assert result.exit_code == 0
-    assert (
-        "coordinates: [('2.0', '1.9'), ('2.5', '1.5'), ('3.3', '7.3')]" in result.output
-    )
+    assert "Found the treasure at: (3.3, 7.3)" in result.output
